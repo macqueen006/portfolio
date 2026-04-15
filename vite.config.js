6e794e0308b59eecd3ab2cd084e629ca9eab35f7
macqueen006 <ogbrukomogodwin@gmail.com>
2026-04-15 18:29:44 +0100

    fix the details links

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,24 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/portfolio/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html",
-        about: "about.html",
-        contact: "contact.html",
-        blog: "blog.html",
-        projects: "projects.html",
-      },
+    base: "/portfolio/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: "index.html",
+                about: "about.html",
+                contact: "contact.html",
+                blog: "blog.html",
+                projects: "projects.html",
+                debbo: "details/debbo.html",
+                librana: "details/librana.html",
+                "made-in-nigeria": "details/made-in-nigeria.html",
+                semechcorp: "details/semechcorp.html",
+                tracewaste: "details/trace-waste.html",
+                "w3eco-friendly": "details/w3eco-friendly.html",
+            },
+        },
     },
-  },
-  plugins: [tailwindcss()],
+    plugins: [tailwindcss()],
 });
